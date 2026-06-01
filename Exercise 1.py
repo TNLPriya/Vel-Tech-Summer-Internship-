ba1ae53464b69cb85ebe80ac40c0a22a6195aeb7
TNLPriya <vtu30396@veltech.edu.in>
2026-06-01 14:29:03 +0530

    Create Exercise 1.py

---
+++ Exercise 1.py
@@ -0,0 +1,6 @@
+name = "Naga Lakshmi Priya"
+age = 20
+college = "Veltech University"
+branch = "Computer Science and Engineering"
+cgpa = 8.9
+print(f"My name is {name}, I am {age} studying {branch} at {college} with a CGPA of {cgpa}.")
